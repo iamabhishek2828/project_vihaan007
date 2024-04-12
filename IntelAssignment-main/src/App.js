701380311fbda1073eb4cccce3b4c644c6263bfa
kanishk-8 <kanishkkumar222004@gmail.com>
2024-04-12 16:33:43 +0530

    update1

--- IntelAssignment-main/src/App.js
+++ IntelAssignment-main/src/App.js
@@ -15,8 +15,8 @@ function App() {
       path: "/",
       element: (
         <>
-          {/* <Navbar /> */}
-          <LoadingPage />
+          <Navbar />
+          <Home />
         </>
       ),
     },
--- IntelAssignment-main/src/components/Navbar.js
+++ IntelAssignment-main/src/components/Navbar.js
@@ -7,12 +7,14 @@ import { Link } from "react-router-dom";
 
 function BasicExample() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="border-body">
       <Container>
-        <Navbar.Brand to="/Home">CureTech</Navbar.Brand>
+        <Navbar.Brand to="/Home">
+          <h1>CureTech</h1>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="me-auto ">
             <Nav.Link as={Link} to="/Home">
               Home
             </Nav.Link>
